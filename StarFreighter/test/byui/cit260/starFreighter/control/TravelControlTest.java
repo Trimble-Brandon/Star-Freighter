307deb0e562cc8ba39a47e792bac4e077f7bec86
Nixon-Joseph <Nieminen432@gmail.com>
2016-02-27 20:03:23 -0700

    Updated Test cases for TravelContol

--- StarFreighter/test/byui/cit260/starFreighter/control/TravelControlTest.java
+++ StarFreighter/test/byui/cit260/starFreighter/control/TravelControlTest.java
@@ -23,13 +23,55 @@ public class TravelControlTest {
     @Test
     public void testCalcTime() {
         System.out.println("calcTime");
-        int skill = 10;
-        int upgrades = 10;
-        int distance = 30;
+        int skill = 2;
+        int upgrades = 2;
+        int distance = 7;
         TravelControl instance = new TravelControl();
-        int expResult = 10;
+        int expResult = 4;
         int result = instance.calcTime(skill, upgrades, distance);
         System.out.println(result + " -> " + expResult);
+        
+        skill = -7;
+        upgrades = 4;
+        distance = 7;
+        expResult = -1;
+        result = instance.calcTime(skill, upgrades, distance);
+        System.out.println("\n" + result + " -> " + expResult);
+        
+        skill = 5;
+        upgrades = 6;
+        distance = -4;
+        expResult = -1;
+        result = instance.calcTime(skill, upgrades, distance);
+        System.out.println("\n" + result + " -> " + expResult);
+        
+        skill = 11;
+        upgrades = -1;
+        distance = -98;
+        expResult = -1;
+        result = instance.calcTime(skill, upgrades, distance);
+        System.out.println("\n" + result + " -> " + expResult);
+        
+        skill = 1;
+        upgrades = 10;
+        distance = 25;
+        expResult = 14;
+        result = instance.calcTime(skill, upgrades, distance);
+        System.out.println("\n" + result + " -> " + expResult);
+        
+        skill = 10;
+        upgrades = 0;
+        distance = 1;
+        expResult = 0;
+        result = instance.calcTime(skill, upgrades, distance);
+        System.out.println("\n" + result + " -> " + expResult);
+        
+        skill = 10;
+        upgrades = 10;
+        distance = 30;
+        expResult = 10;
+        result = instance.calcTime(skill, upgrades, distance);
+        System.out.println("\n" + result + " -> " + expResult);
     }
     
 }
