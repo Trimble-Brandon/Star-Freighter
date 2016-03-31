b2be7fbb7a544f1d4ac41e26f26829fe0241b26f
Brandon <Brandon@10.0.0.74>
2016-03-31 17:00:04 -0600

    Updated GameMenuView with correct options, better layout and correct cases. Needs implementing of CharacterMenuView, MapView, JobBoardView, and potentially an InventoryView

--- StarFreighter/src/byui/cit260/starFreighter/view/GameMenuView.java
+++ StarFreighter/src/byui/cit260/starFreighter/view/GameMenuView.java
@@ -15,11 +15,17 @@ import byui.cit260.starFreighter.model.InventoryItem;
 class GameMenuView extends View
 {    
     public GameMenuView() {
-        super("\nTesting LocationChooserView, ShopMenuView. "
-                + "\nEnter L for LocationChooserView, "
-                + "\nEnter S for ShopMenuView, "
-                + "\nEnter D for DisplayInventory, "
-                + "\nEnter N to return ");
+        super("\n"
+                + "\n----------------------------------------------------------"
+                + "\n| Game Menu Options |                                    |"
+                + "\nL - travel to new location!                              |"
+                + "\nM - see your map!                                        |" 
+                + "\nS - enter the Shop and see repairs/upgrades              |"
+                + "\nJ - view the Job board                                   |" 
+                + "\nI - view your Inventory                                  |"
+                + "\nC - the Character Menu                                   |"
+                + "\nQ - view main menu                                       |"
+                + "\n---------------------------------------------------------|");
     }
      
      public boolean doAction(String value) {
@@ -28,13 +34,22 @@ class GameMenuView extends View
                 LocationChooserView lChooser = new LocationChooserView();
                 lChooser.display();
                 break;
+            case "M":
+                System.out.println("Placeholder for Map");
+                break;
             case "S":
                 ShopMenuView sMenu = new ShopMenuView();
                 sMenu.display();
                 break;
-            case "D":
+            case "J":
+                System.out.println("Placeholder for Job board");
+                break;
+            case "I":
+                System.out.println("Placeholder for Inventory view");
                 this.displayInventory();
                 break;
+            case "C":
+                System.out.println("Placeholder for Character menu view");
             case "N":
                 return true;
             default:
